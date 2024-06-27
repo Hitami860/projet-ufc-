a3d911514a6d8f3c3f125ab28b2bd1d7b7e9e754
Hitami860 <Hitami86000@gmail.com>
2024-06-27 16:32:20 +0200

    update

---
+++ json/script.js
@@ -0,0 +1,31 @@
+
+async function afficherName() {
+
+    const reponse = await fetch("json/Fighters.json");
+    const name = await reponse.json();
+
+    new gridjs.Grid({
+        columns: [{
+            id: 'Nickname',
+            name: 'Peudonyme'
+        }, {
+            id: 'FighterId',
+            name: 'ID'
+        }, {
+            id: 'Wins',
+            name: 'Victoire'
+        }, {
+            id: 'Losses',
+            name: 'Défaite'
+        }],
+
+        search: true,
+
+        pagination: true,
+        limit: 15,
+
+        data: name,
+
+    }).render(document.getElementById('tableau'));
+}
+afficherName()
