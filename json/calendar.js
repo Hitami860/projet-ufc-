5974ff64f9e93b3a3f5f74c3821f8ffa6295ebc4
Hitami860 <Hitami86000@gmail.com>
2024-07-02 14:58:50 +0200

    update

--- json/calendar.js
+++ json/calendar.js
@@ -1,23 +1,21 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
+
+  let addevent = [];
+ fetch('./json/datecombatufc.json').then(res => res.json()).then(data => {
+    for (i = 0; i < data.length; i++) {
+      addevent.push({title : data[i].Name, start : data[i].DateTime})
+    }
+
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
+      initialView: 'dayGridMonth',
+      events: addevent,
+      selectable: true,
     });
     calendar.render();
-  });
+  })
+
+});
